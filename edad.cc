80f4a9cc0cdda634a1659b90b4ed55d55e446458
vale y sofiy mami <3015609739v@gmail.com>
2026-05-13 18:42:36 -0500

    subiedno ejercicio basico

---
+++ edad.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int edad;
+
+    cout << "Escribe tu edad: ";
+    cin >> edad;
+
+    cout << "tu edad es "<< edad <<" años";
+
+}
