5a6993d1e203afc81d341c0c5d6ddc04d4f04409
vale y sofiy mami <3015609739v@gmail.com>
2026-05-13 18:57:21 -0500

    ejercicio basico

--- nombre.cc
+++ nombre.cc
@@ -1,1 +1,13 @@
-#include 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string nombre;
+
+    cout << "escribe tu nombre";
+    cin >> nombre;
+
+    cout << " hola " << nombre <<endl;
+    cout << " encantado de conocerte"; 
+}
